eca83532ebe12008ad6ac19e493986bc4466d59b
Stijn Van Hulle <stijn@stijnvanhulle.be>
2026-04-24 00:34:28 +0200

    update packages

---
+++ examples/client/src/gen/models/ts/storeController/index.ts
@@ -0,0 +1,26 @@
+export type {
+  DeleteOrderPathOrderId,
+  DeleteOrderRequestConfig,
+  DeleteOrderResponse,
+  DeleteOrderResponses,
+  DeleteOrderStatus400,
+  DeleteOrderStatus404,
+} from './DeleteOrder.js'
+export type { GetInventoryRequestConfig, GetInventoryResponse, GetInventoryResponses, GetInventoryStatus200 } from './GetInventory.js'
+export type {
+  GetOrderByIdPathOrderId,
+  GetOrderByIdRequestConfig,
+  GetOrderByIdResponse,
+  GetOrderByIdResponses,
+  GetOrderByIdStatus200,
+  GetOrderByIdStatus400,
+  GetOrderByIdStatus404,
+} from './GetOrderById.js'
+export type { PlaceOrderRequestConfig, PlaceOrderResponse, PlaceOrderResponses, PlaceOrderStatus200, PlaceOrderStatus405 } from './PlaceOrder.js'
+export type {
+  PlaceOrderPatchRequestConfig,
+  PlaceOrderPatchResponse,
+  PlaceOrderPatchResponses,
+  PlaceOrderPatchStatus200,
+  PlaceOrderPatchStatus405,
+} from './PlaceOrderPatch.js'
---
+++ examples/zod/src/gen3/index.ts
@@ -0,0 +1,5 @@
+export { createItemResponseSchema, createItemStatus200Schema } from './zod/createItemSchema.ts'
+export { itemSchema } from './zod/itemSchema.ts'
+export { itemTypeASchema } from './zod/itemTypeASchema.ts'
+export { itemTypeBSchema } from './zod/itemTypeBSchema.ts'
+export { updateItemResponseSchema, updateItemStatus200Schema } from './zod/updateItemSchema.ts'
