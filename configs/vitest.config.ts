40b7944939e9d0c623a8243d42445cb2c52d94dd
Stijn Van Hulle <stijn@stijnvanhulle.be>
2026-05-04 21:08:41 +0200

    cleanup vitest

--- configs/vitest.config.ts
+++ configs/vitest.config.ts
@@ -1,17 +1,8 @@
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
-
 import { defineConfig } from 'vitest/config'
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   resolve: {
     tsconfigPaths: true,
-    alias: {
-      '@internals/utils': path.resolve(__dirname, '../internals/utils/src/index.ts'),
-      '@internals/tanstack-query': path.resolve(__dirname, '../internals/tanstack-query/src/index.ts'),
-    },
   },
   test: {
     testTimeout: 30000,
