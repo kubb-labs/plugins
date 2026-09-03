a135eba727487f8bd9444e5198a018678465107a
stijnvanhulle <5904681+stijnvanhulle@users.noreply.github.com>
2026-09-03 13:55:21 +0000

    chore(examples): update generated files

--- examples/advanced/src/gen/msw/pets/createPetsHandler.ts
+++ examples/advanced/src/gen/msw/pets/createPetsHandler.ts
@@ -2,24 +2,18 @@ import type { CreatePetsResponse, CreatePetsBody } from '../../models/ts/pets/Cr
 import type { HttpResponseResolver } from 'msw'
 import { http } from 'msw'
 
-export function createPetsHandlerResponse201(data: CreatePetsResponse) {
+export function createPetsHandlerResponse201(data?: CreatePetsResponse) {
   return new Response(JSON.stringify(data), {
     status: 201,
-    headers: {
-      'Content-Type': 'application/json',
-    },
   })
 }
 
-export function createPetsHandler(data?: CreatePetsResponse | HttpResponseResolver<Record<string, string>, CreatePetsBody>) {
+export function createPetsHandler(data?: string | number | boolean | null | object | HttpResponseResolver<Record<string, string>, CreatePetsBody>) {
   return http.post<Record<string, string>, CreatePetsBody>(`/pets/:uuid`, function handler(info) {
     if (typeof data === 'function') return data(info)
 
     return new Response(JSON.stringify(data), {
       status: 201,
-      headers: {
-        'Content-Type': 'application/json',
-      },
     })
   })
 }
