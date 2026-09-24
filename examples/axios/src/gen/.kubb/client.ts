e0ca49bf9040f8a105a7442f34d5798b21bfb3be
stijnvanhulle <5904681+stijnvanhulle@users.noreply.github.com>
2026-09-24 10:53:26 +0000

    chore(examples): update generated files

--- examples/axios/src/gen/.kubb/client.ts
+++ examples/axios/src/gen/.kubb/client.ts
@@ -1,8 +1,9 @@
 import axios from 'axios'
+import type { HeadersInit, PathParamStyle, PathSerializer, Serializers, Styles } from './serializers'
+import type { StandardSchemaValidator } from './standardSchema'
 import type { AxiosError, AxiosInstance, AxiosRequestConfig, AxiosResponse, InternalAxiosRequestConfig } from 'axios'
-import { applyHeaderStyles, defaultBodySerializer, defaultPathSerializer, defaultQuerySerializer, isDefaultJsonBody, serializeCookies } from './serializers.ts'
-import type { HeadersInit, PathParamStyle, PathSerializer, Serializers, Styles } from './serializers.ts'
-import { ParseError, type StandardSchemaValidator, validateStandardSchema } from './standardSchema.ts'
+import { applyHeaderStyles, defaultBodySerializer, defaultPathSerializer, defaultQuerySerializer, isDefaultJsonBody, serializeCookies } from './serializers'
+import { ParseError, validateStandardSchema } from './standardSchema'
 
 /**
  * HTTP status codes treated as a success, everything else is an error.
